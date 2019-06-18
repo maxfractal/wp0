7ee684bd14de502f94f3e7f306da8bd1718a3963
Xris <cp@maxfractal.com>
2019-06-18 16:31:48 -0400

    Adding less to the grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,16 @@ module.exports = function (grunt) {
     grunt.initConfig({
         jshint: ['Gruntfile.js']
     });
+    grunt.initConfig({
+        less: {
+            compile: {
+                files: {
+                    'build/assets/css/compiled.css': 'less/layout.less'
+                }
+            }
+        }
+    });
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.registerTask('default', ['jshint']); // register a default task alias
 };
