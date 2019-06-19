945f88096e6b98f79ed4a6cb067ce11262aa6586
Xris <cp@maxfractal.com>
2019-06-19 10:36:41 -0400

    Fixed grunt tasks less and concat working together, jshint not working.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,21 @@ module.exports = function (grunt) {
         less: {
             compile: {
                 files: {
-                    'build/assets/css/compiled.css': 'less/*.less'
+                    'build/assets/css/compiled.css': 'less/**/*.less'
+                }
+            }
+        },
+        concat: {
+            js: {
+                files: {
+                    'build/assets/js/bundle.js': 'js/**/*.less'
                 }
             }
         }
     });
+
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.registerTask('default', ['jshint']); // register a default task alias
 };
---
+++ README.md
@@ -0,0 +1,20 @@
+Project: wp0
+
+
+Install:
+1. Install NPM
+2. npm install --save-dev grunt-contrib-jshint
+3. npm install grunt-contrib-less --save-dev
+#grunt-contrib-concat
+4. npm install grunt-contrib-concat --save-dev
+
+
+Grunt commands:
+
+#compiles all less files
+grunt less
+
+#compiles specific files
+grunt less:compile
+
+
