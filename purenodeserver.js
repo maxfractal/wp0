1cd2e3f482a1668c3f6d8faf7d2f4eeaf7a844de
Xris <cp@maxfractal.com>
2019-07-12 11:03:19 -0400

    First route in purenodeserver

--- purenodeserver.js
+++ purenodeserver.js
@@ -1,10 +1,24 @@
 const http = require('http');
+const url = require('url');
 
 function handler(req, res) {
-    res.writeHead(200, {'Content-type':'text/plain'});
-    res.write('Hello I am web server!');
-    res.end();
 
+
+    const parsedUrl = url.parse(req.url,true);
+    //console.log(parsedUrl);
+
+    if(parsedUrl.pathname === '/') {
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write('Hello I am web server!');
+        return res.end();
+    } else if (parsedUrl.pathname === '/time') {
+        res.writeHead(200,{'Content-type':'text/plain'});
+        res.write(new Date().toString());
+        return res.end();
+    } else {
+        res.writeHead(400, {'Content-type':'text/plain'});
+        return res.end();
+    }
 }
 
 const server = http.createServer(handler);
