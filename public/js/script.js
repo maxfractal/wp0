741772346c11a67efba35b4580ecf05c05c28de0
Xris <cp@maxfractal.com>
2019-09-01 15:05:12 -0400

    Adding Script.js and broke it out to two placedholder functions.

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,22 @@
 //script.js
 //TODO add some code here.
+
+//JQuery
+// $(document).ready(function() {
+//     alert('Ready');
+// });
+
+//Vanilla JS
+document.addEventListener("DOMContentLoaded", function(){
+    start();
+    anim();
+});
+
+function start() {
+    alert("Pure JS Ready");
+}
+
+function anim() {
+    alert("Placeholder for animation script.");
+    console.log('Placeholder anim()');
+}
